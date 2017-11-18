336125ab7b8cc219fd75dfee60d8bc299490cc8e
lithien <speck@tfwno.gf>
2017-11-18 21:27:32 +1100

    post objects
    switched to a more object-based handling of posts and drawing posts

--- kumochan.js
+++ kumochan.js
@@ -1,27 +1,39 @@
 var kumo = SVG("kumochan").size("100%", "100%"); // "kumo" is nippon for "spider".
 
-var rectangle = kumo.rect(100,100).attr( {fill: "#FF0000"} );
+function post(postID, postContent, timeStamp) {
+	this.ID = postID;
+	this.content = postContent;
+	this.timeStamp = timeStamp;
+}
 
-function drawPost(postID, postContent, timeStamp, xpos, ypos) {
-	
+function drawPost(post, xpos, ypos) {
+	var minWidth = 400;
+	var minHeight = 200;
 	// draw the post's text content
-	var content = kumo.text(postContent).fill("#EEEEEE");
-	content.center(0, 0);
+	var content = kumo.text(post.content).fill("#EEEEEE").center(0, 0);
 	
 	// grab the width and height of the text we just drew...
 	var width = content.bbox().width;
 	var height = content.bbox().height;
+	
+	// check if the width and height are less than the minimums, set them if they are.
+	width = (width < minWidth) ? minWidth : width + 50;
+	height = (height < minHeight) ? minHeight: height + 50;
+	
 	console.log(width + ", " + height);
 	
-	// ...and use it to make the container
-	var container = kumo.ellipse(width + 100, height + 100).fill("#444444");
-	container.center(0, 0).back();
+	// ...and use them to make the container
+	var container = kumo.ellipse(width, height).fill("#444444").center(0, 0).back();
 	
-	var post = kumo.group().add(container).add(content);
-	post.center(xpos, ypos);
+	var post = kumo.group().add(container).add(content).center(xpos, ypos);
 }
 
-drawPost("1234567890", "this is a test string!", "timestamp", 500, 250);
-drawPost("1234567891", "this is another test string!\nthis second line is long, so the ellipse is wider!", "timestamp", 750, 500);
-drawPost("1234567892", "this is the final string!\nthere's a bunch\nof line breaks\nso the ellipse\nis taller\nto compensate.", "timestamp", 1000, 250);
-drawPost("1234567893", "it might look better to keep a set ratio so each ellipse is the same shape.", "timestamp", 1000, 750);
+var test1 = new post("1234567890", "this is a test string!", "timestamp");
+var test2 = new post("1234567891", "this is another test string!\nthis second line is long, so the ellipse is wider!", "timestamp");
+var test3 = new post("1234567892", "this is a tall string!\nss\nss\nss\nss\nss\nss\nss\nss\nss\nss\nss", "timestamp");
+var test4 = new post("1234567893", "it might look better to keep a set ratio so each ellipse is the same shape.", "timestamp");
+
+drawPost(test1, 500, 250);
+drawPost(test2, 750, 500);
+drawPost(test3, 1000, 250);
+drawPost(test4, 1000, 750);
